6667e2e6b9b01f2f5ad572d51fa062968437ea78
Hamed Ghazali <hamed.ghazali@digikala.com>
2024-04-21 12:46:46 +0330

    cleanup and type safety

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import { useState } from "react";
 import "./App.css";
 import { useReqHook } from "./hooks";
-import type { Post } from "./types";
+import type { Post, Comment } from "./types";
 
 function App() {
   const [count, setCount] = useState(0);
-  const { data, onRefetch, loading } = useReqHook<Post[]>("/posts");
+  const {
+    data: postsData,
+    onRefetch: onRefetchPosts,
+    loading: isPostsLoading,
+  } = useReqHook<Post[]>("/posts");
+  const {
+    data: commentsData,
+    onRefetch: onRefetchComments,
+    loading: isCommentsLoading,
+  } = useReqHook<Comment[]>("/comments", { params: { postId: 1 } });
 
   return (
     <>
-      {loading && <h1>Loading...</h1>}
+      {isPostsLoading && <h1>Posts Loading...</h1>}
       <div className="card">
-        {data?.map((i) => (
+        <h2>Posts</h2>
+        {postsData?.map((i) => (
+          <pre key={i.id}>{JSON.stringify(i, null, 2)}</pre>
+        ))}
+      </div>
+
+      {isCommentsLoading && <h1>Comments Loading...</h1>}
+      <div className="card">
+        <h2>Comments</h2>
+        {commentsData?.map((i) => (
           <pre key={i.id}>{JSON.stringify(i, null, 2)}</pre>
         ))}
       </div>
 
       <div className="card">
-        <button onClick={onRefetch} disabled={loading}>
-          refetch
+        <button onClick={onRefetchPosts} disabled={isPostsLoading}>
+          refetch posts
+        </button>
+        <button onClick={onRefetchComments} disabled={isCommentsLoading}>
+          refetch comments
         </button>
         <p>{count}</p>
+
+        {/* for rendering test */}
         <button onClick={() => setCount((prev) => prev - 1)}>-</button>
         <button onClick={() => setCount((prev) => prev + 1)}>+</button>
       </div>
